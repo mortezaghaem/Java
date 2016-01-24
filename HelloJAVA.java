ef5298bc0cec478cd5e786c85d02aae57d939087
mortezaghaem <mortezaghaem@gmail.com>
2016-01-24 18:02:23 +0330

    This is hello world to java

---
+++ HelloJAVA.java
@@ -0,0 +1,13 @@
+
+public class Hellojava {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Hello java");
+
+    }
+
+}
